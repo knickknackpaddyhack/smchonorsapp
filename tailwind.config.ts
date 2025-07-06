0c450616f7481552191d133695c466e962e0c731
Connor Mccaslin <crm20@stmarys-ca.edu>
2025-07-06 16:29:22 +0000

    Replace Playfair Display with Arpona font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Montserrat', 'sans-serif'],
-        headline: ['Playfair Display', 'serif'],
+        headline: ['Arpona', 'Cormorant Garamond', 'serif'],
         code: ['monospace'],
       },
       colors: {
