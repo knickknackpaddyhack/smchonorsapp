5537fac010b4b4f779e3424c27b509ce1c1e7aaf
Connor Mccaslin <crm20@stmarys-ca.edu>
2025-07-06 17:31:18 +0000

    It now says my "apiKey", "authDomain", "projectId", "storageBucket", "me

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 import type {NextConfig} from 'next';
 
+// Explicitly load env variables from the .env file in the root directory.
+require('dotenv').config();
+
 const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -23,11 +18,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-    ],
-  },
-  devIndicators: {
-    allowedDevOrigins: [
-      'https://6000-firebase-studio-1751643442833.cluster-2xfkbshw5rfguuk5qupw267afs.cloudworkstations.dev',
     ],
   },
 };
