c106e0807fad2f6acd45852f574f871df4d41431
Connor Mccaslin <crm20@stmarys-ca.edu>
2025-07-08 11:55:37 +0000

    I attempted to log in and received the following toast error message bef

--- src/contexts/auth-context.tsx
+++ src/contexts/auth-context.tsx
@@ -41,6 +41,9 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     }
     try {
       const provider = new GoogleAuthProvider();
+      provider.setCustomParameters({
+        prompt: 'select_account'
+      });
       await signInWithPopup(auth, provider);
     } catch (error: any) {
         console.error("Authentication Error Details:", error);
